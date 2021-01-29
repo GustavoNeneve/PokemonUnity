edd5569234aa1a2e58804a00a8cd3ec8280ec9eb
GustavoNeneve <47082428+GustavoNeneve@users.noreply.github.com>
2021-01-29 16:04:30 -0300

    ui x5

---
+++ Pokemon Unity/Assets/Scripts/Title.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title : MonoBehaviour {
+
+	public float speed = 1f;
+	public GameObject TitleMain;
+	public GameObject Menu;
+	public float pixeljump;
+	public Vector3 StartPosition;
+
+	void Start() {
+		StartPosition = transform.position;
+	}
+	void Awake() 
+	{
+		TitleMain.SetActive(true);
+		Menu.SetActive(false);
+	}
+
+	void FixedUpdate() {
+		//float newPos = Mathf.Repeat(Time.time * speed, StartPosition);
+		float newPos = Mathf.Repeat(Time.time * speed, pixeljump);
+		
+		transform.position = StartPosition + Vector3.left * newPos;
+
+		
+
+	}
+
+	void Update() 
+	{
+	    if (Input.anyKey)
+        {
+
+            //TitleMain.SetActive(false);
+			timer();
+			bool isActive = TitleMain.activeSelf;
+ 
+            TitleMain.SetActive(!isActive);
+			
+        }
+
+
+		Menu.SetActive(!TitleMain.activeSelf);
+
+		
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			bool isActive = false;
+
+			TitleMain.SetActive(!isActive);
+		}
+	
+	
+	}
+
+	IEnumerator timer()
+	{
+		yield return new WaitForSeconds(0.25f);
+	}
+
+
+
+}
--- TOOLS/Tiled2PkUnity/Launch.bat
+++ TOOLS/Tiled2PkUnity/Launch.bat
@@ -1,9 +1,3 @@
 @echo off
-IF [%1]==[] (
-    echo You must drag and drop your tilemap's JSON onto this batch script.
-    echo Otherwise, execute Tiled2PkUnity in the command prompt with your arguments.
-    pause
-) ELSE (
-    node tiled2pkunity.js %1
-    pause
-)
+node tiled2pkunity.js ./tileMAP.json ./tileSET.json collision.txt 1
+pause
